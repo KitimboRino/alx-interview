9ab761fa7cf950a1af6270873c891f70e13303eb
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-08-08 20:04:51 +0300

    fix: 0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,27 +7,27 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 const getCharacterName = (characterUrl) => {
-    return new Promise((resolve, reject) => {
-        request(characterUrl, (err, res, body) => {
-            if (err) return reject(err);
-            resolve(JSON.parse(body).name);
-        });
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (err, res, body) => {
+      if (err) return reject(err);
+      resolve(JSON.parse(body).name);
     });
+  });
 };
 
 request(url, async (err, res, body) => {
-    if (err) {
-        console.error(`Error fetching movie data: ${err}`);
-        return;
-    }
+  if (err) {
+    console.error(`Error fetching movie data: ${err}`);
+    return;
+  }
 
-    try {
-        const charactersArray = JSON.parse(body).characters;
-        for (const character of charactersArray) {
-            const name = await getCharacterName(character);
-            console.log(name);
-        }
-    } catch (err) {
-        console.error(`Error processing character data: ${err}`);
+  try {
+    const charactersArray = JSON.parse(body).characters;
+    for (const character of charactersArray) {
+      const name = await getCharacterName(character);
+      console.log(name);
     }
+  } catch (err) {
+    console.error(`Error processing character data: ${err}`);
+  }
 });
