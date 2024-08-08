16a56898410257d7861dd5ba7917c1ef27885f62
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-08-08 20:00:21 +0300

    task: 0. Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/* prints all characters of Star Wars movie passed as command-line argument */
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+const getCharacterName = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+        request(characterUrl, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(JSON.parse(body).name);
+        });
+    });
+};
+
+request(url, async (err, res, body) => {
+    if (err) {
+        console.error(`Error fetching movie data: ${err}`);
+        return;
+    }
+
+    try {
+        const charactersArray = JSON.parse(body).characters;
+        for (const character of charactersArray) {
+            const name = await getCharacterName(character);
+            console.log(name);
+        }
+    } catch (err) {
+        console.error(`Error processing character data: ${err}`);
+    }
+});
